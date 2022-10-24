85cee89c6b78491f686133483d72d4acbbc8e907
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2022-10-23 17:53:41 -0700

    cleaned up unused files. created main.theme.js and wrapped around App component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import { mainTheme } from './styles/themes/main.theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={mainTheme}>
+      <Box
+        sx={{
+          height: '100vh',
+          width: '100vw',
+          bgcolor: 'primary.main',
+        }}
+      >
+        UNDERTAKER
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/styles/themes/main.theme.js
@@ -0,0 +1,10 @@
+import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+export const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
