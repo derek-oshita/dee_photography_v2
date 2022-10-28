b1df04a543a8410b84ddc3ece76ffd76da89e052
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2022-10-28 11:30:27 -0700

    whoops, navbar commented out here.

--- client/src/components/view/LandingView.js
+++ client/src/components/view/LandingView.js
@@ -3,15 +3,6 @@ import { Navbar } from '../content/Navbar';
 
 export const LandingView = () => {
   return (
-    // <Box
-    //   sx={{ bgcolor: 'background.default', height: '100vh', width: '100vw' }}
-    // >
-    //   <Navbar>
-
-    //   </Navbar>
-
-    // </Box>
-
     <>
       <Navbar />
     </>
